9fd488c2f5bd87f96f4715cc1f6a4883a1eb5d10
Snakeheart404 <polina.glazunova@yahoo.com>
2021-12-31 15:41:00 +0200

    new spinner

---
+++ src/components/Loader.svelte
@@ -0,0 +1,20 @@
+<script>
+    import {Circle} from 'svelte-loading-spinners'
+</script>
+<div class="loader">
+    <Circle color="#f44336"/>
+</div>
+
+<style>
+    .loader {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        background-color: rgba(255,255,255,0.5);
+    }
+</style>

fd1580620cd25347a7de3fc1df29bce6b55e4d13
Snakeheart404 <polina.glazunova@yahoo.com>
2022-01-13 21:22:58 +0200

    fix

--- src/App.svelte
+++ src/App.svelte
@@ -6,6 +6,8 @@
   import { WebSocketLink } from "@apollo/client/link/ws";
   import { errorMessage, loadersCount } from "./stores.js";
 
+  let online;
+
   function createApolloClient() {
     const wsLink = new WebSocketLink({
       uri: uri_from_env,
@@ -69,42 +71,76 @@
   };
 </script>
 
-<main>
-  {#if $frogsArray.loading || $loadersCount}
-    <Loader />
-  {:else if $frogsArray.error}
-    <h1>Error: {$frogsArray.error.message}</h1>
-  {:else if $frogsArray.data}
-    <input bind:value={newFrogInfo.name} placeholder="Name" />
-    <input bind:value={newFrogInfo.count} placeholder="Count" />
-    <button on:click={AddFrog}>Add frog</button>
+<svelte:window bind:online />
 
-    {#if $frogsArray.data.FrogsDB_frogs.length != 0 && $frogsArray.data.FrogsDB_frogs}
-      <table border="1">
-        <caption>Frogs</caption>
-        <tr>
-          <th>Name</th>
-          <th>Count</th>
-          <th>Deletion</th>
-        </tr>
-        {#each $frogsArray.data.FrogsDB_frogs as frog (frog.id)}
+<main>
+  {#if online}
+    {#if $frogsArray.loading || $loadersCount}
+      <Loader />
+    {:else if $frogsArray.error}
+      <h1>Error: {$frogsArray.error.message}</h1>
+    {:else if $frogsArray.data}
+      <input bind:value={newFrogInfo.name} placeholder="Name" />
+      <input bind:value={newFrogInfo.count} placeholder="Count" />
+      <button on:click={AddFrog}>Add frog</button>
+
+      {#if $frogsArray.data.FrogsDB_frogs.length != 0 && $frogsArray.data.FrogsDB_frogs}
+        <table border="1">
+          <caption>Frogs</caption>
           <tr>
-            <td>{frog.name}</td>
-            <td>{frog.count}</td>
-            <td><button on:click={RemoveFrogs(frog.id)}>Delete</button></td>
+            <th>Name</th>
+            <th>Count</th>
+            <th>Deletion</th>
           </tr>
-        {/each}
-      </table>
-    {:else}
-      <h1>No frogs</h1>
+          {#each $frogsArray.data.FrogsDB_frogs as frog (frog.id)}
+            <tr>
+              <td>{frog.name}</td>
+              <td>{frog.count}</td>
+              <td><button on:click={RemoveFrogs(frog.id)}>Delete</button></td>
+            </tr>
+          {/each}
+        </table>
+      {:else}
+        <h1>No frogs</h1>
+      {/if}
+      <div class="errorLabel">{$errorMessage}</div>
     {/if}
-    <div class="errorLabel">{$errorMessage}</div>
+  {:else}
+    <div class="overlay">
+      <h1>Internet connection is lost! :(</h1>
+      <div class="overlay background" />
+    </div>
   {/if}
 </main>
 
 <style>
+  :root {
+    --background: rgb(195 195 195);
+  }
+
   main {
     padding: 0;
     margin: 0;
+  }
+
+  .overlay {
+    position: fixed;
+    z-index: 0;
+    top: 0;
+    left: 0;
+    display: flex;
+    width: 100vw;
+    height: 100%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .overlay .background {
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    background-color: var(--gray-bg);
+    opacity: 0.5;
   }
 </style>
